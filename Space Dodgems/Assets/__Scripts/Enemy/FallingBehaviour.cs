7a14a4d864eb122e80c74a6fcd2ed4cc913a294b
Rachel <g00337231@gmit.ie>
2020-03-09 17:17:11 +0000

    adding falling enemey

---
+++ Space Dodgems/Assets/__Scripts/Enemy/FallingBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FallingBehaviour : MonoBehaviour
+{
+    // == private fields ==
+    [SerializeField]
+    private float speed;
+
+    Scene currentScene;
+    string sceneName;
+
+    private Rigidbody2D rb; //rigid body component
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2.0f;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.down * speed;
+    }
+
+}
