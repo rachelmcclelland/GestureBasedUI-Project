e46a53e13608cfb58e9ceed5b5b18c5eb215a954
Rachel <g00337231@gmit.ie>
2020-04-01 11:47:18 +0100

    adding voice recognition for pausing game

---
+++ Space Dodgems/Assets/__Scripts/GameControls/VoiceControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+// adapted from https://www.youtube.com/watch?v=29vyEOgsW8s
+public class VoiceControl : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    void Start()
+    {
+        actions.Add("pause", PauseGame);
+        actions.Add("pause game", PauseGame);
+        actions.Add("stop", PauseGame);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+
+        //starts listening
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void PauseGame()
+    {
+        Debug.Log("Pausing Game");
+    }
+
+}
