46c1b69e1f88a64b4eecb15c42334bfd5da37d44
Rachel <g00337231@gmit.ie>
2020-04-01 12:24:37 +0100

    working pause menu

---
+++ Space Dodgems/Assets/MenuScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// adpated from https://www.sitepoint.com/adding-pause-main-menu-and-game-over-screens-in-unity/
+public class MenuScript : MonoBehaviour
+{
+    GameObject[] pauseObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+    }
+
+    public void Reload()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    //controls the pausing of the scene
+    public void pauseControl()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    //loads inputted level
+    public void LoadLevel(string level)
+    {
+        Application.LoadLevel(level);
+    }
+
+}
--- Space Dodgems/Assets/__Scripts/GameControls/VoiceControl.cs
+++ Space Dodgems/Assets/__Scripts/GameControls/VoiceControl.cs
@@ -8,11 +8,17 @@ using System.Linq;
 // adapted from https://www.youtube.com/watch?v=29vyEOgsW8s
 public class VoiceControl : MonoBehaviour
 {
+    GameObject[] pauseObjects;
+
     private KeywordRecognizer keywordRecognizer;
     private Dictionary<string, Action> actions = new Dictionary<string, Action>();
 
     void Start()
     {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+
         actions.Add("pause", PauseGame);
         actions.Add("pause game", PauseGame);
         actions.Add("stop", PauseGame);
@@ -33,6 +39,26 @@ public class VoiceControl : MonoBehaviour
     private void PauseGame()
     {
         Debug.Log("Pausing Game");
+        Time.timeScale = 0;
+        showPaused();
     }
 
+    //shows objects with ShowOnPause tag
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            Debug.Log(g);
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
 }
