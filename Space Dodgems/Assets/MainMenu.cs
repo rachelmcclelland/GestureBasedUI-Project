a3e2d4e7ccbb8eaf8b7d341e90acff036d49ea6d
Rachel <g00337231@gmit.ie>
2020-04-01 19:36:56 +0100

    added voice control to start screen

--- Space Dodgems/Assets/MainMenu.cs
+++ Space Dodgems/Assets/MainMenu.cs
@@ -1,10 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actions.Add("start", PlayGame);
+        actions.Add("play game", PlayGame);
+        actions.Add("play", PlayGame);
+        actions.Add("quit", QuitGame);
+        actions.Add("leave", QuitGame);
+        actions.Add("end game", QuitGame);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+
+        //starts listening
+        keywordRecognizer.Start();
+    }
+
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
     public void PlayGame ()
     {
         SceneManager.LoadScene("SpaceDodgems");
